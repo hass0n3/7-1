fe3e591970ff6f21fbad4d18f3102a1e58938b49
hass0n3 <hassen.alfaris@ocsbstudent.ca>
2021-03-24 15:05:36 +0000

    5

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-// This code will display a message if the user correctly guesses the random number
-let userGuess = 0
-let randomNumber = 0
+ display () {
+  user = document.getElementById('input').value
+  user = parseInt(user)
+  ran = Math.floor(Math.random() * 6) + 1
+  document.getElementById('paragraph').innerHTML = ran
 
-document.getElementById('button').addEventListener('click', randomizer)
-
-function randomizer () {
-  // convert userGuess to integer
-  userGuess = document.getElementById('input').value
-  userGuess = parseInt(userGuess)
-
-  // generate random number
-  randomNumber = Math.floor(Math.random() * 6) + 1
-
-  // compare user's guess to random number
-  if (userGuess === randomNumber) {
-    document.getElementById('answer').innerHTML = 'You guessed ' + userGuess + '. That is correct!'
+  if (ran === user) {
+    alert('correct')
   }
 }
